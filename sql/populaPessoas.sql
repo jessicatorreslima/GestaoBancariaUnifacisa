4e302a24defcbbea0d9a2875313b07d77dfaa7e0
Jéssica Torres de Lima <jtlimapro@gmail.com>
2021-06-27 14:47:54 -0300

    Commit final

---
+++ sql/populaPessoas.sql
@@ -0,0 +1,10 @@
+INSERT INTO `unifacisa`.`tb_pessoa` (`cpf`, `data_nascimento`, `nome`) VALUES ('00000000000', '1993-12-07', 'Jéssica');
+INSERT INTO `unifacisa`.`tb_pessoa` (`cpf`, `data_nascimento`, `nome`) VALUES ('00000000001', '1992-08-17', 'Angélica');
+INSERT INTO `unifacisa`.`tb_pessoa` (`cpf`, `data_nascimento`, `nome`) VALUES ('00000000002', '1978-03-10', 'José');
+INSERT INTO `unifacisa`.`tb_pessoa` (`cpf`, `data_nascimento`, `nome`) VALUES ('00000000003', '1956-04-05', 'Joao');
+INSERT INTO `unifacisa`.`tb_pessoa` (`cpf`, `data_nascimento`, `nome`) VALUES ('00000000004', '1998-06-09', 'Pedro');
+INSERT INTO `unifacisa`.`tb_pessoa` (`cpf`, `data_nascimento`, `nome`) VALUES ('00000000005', '1960-08-08', 'Paulo');
+INSERT INTO `unifacisa`.`tb_pessoa` (`cpf`, `data_nascimento`, `nome`) VALUES ('00000000006', '1998-05-07', 'Julia');
+INSERT INTO `unifacisa`.`tb_pessoa` (`cpf`, `data_nascimento`, `nome`) VALUES ('00000000007', '1987-02-24', 'Rafaela');
+INSERT INTO `unifacisa`.`tb_pessoa` (`cpf`, `data_nascimento`, `nome`) VALUES ('00000000008', '1984-09-29', 'Joyce');
+INSERT INTO `unifacisa`.`tb_pessoa` (`cpf`, `data_nascimento`, `nome`) VALUES ('00000000009', '1991-10-09', 'Marcos');
