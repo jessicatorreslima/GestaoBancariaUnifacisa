022020af55cedcbd1ff98b660602a3220b90651b
Jéssica Torres de Lima <jtlimapro@gmail.com>
2021-06-27 14:15:48 -0300

    Documentação formatada

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.unifacisa.banco.controller","l":"ContaController"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.unifacisa.banco.model","l":"Transacao"}];updateSearchResults();
