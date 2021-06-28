3d0496857e8bb8738807546d59a7f476088a457d
Jéssica Torres de Lima <jtlimapro@gmail.com>
2021-06-28 09:11:07 -0300

    Correção do Javadoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.unifacisa.banco.model"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.unifacisa.banco"},{"l":"com.unifacisa.banco.controller"},{"l":"com.unifacisa.banco.model"},{"l":"com.unifacisa.banco.repository"}];updateSearchResults();
